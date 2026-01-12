7b0c4956c8a63e818871e6713a24670ba29004f8
Kioko Muinde <kiokomuinde022@gmail.com>
2026-01-12 23:58:51 +0300

    No one should know what happened here

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3327477814"
+    serviceWorkerVersion: "4104776530"
   }
 });
